06b4edd93aa1acc9d2f20afcdab8574d0c562db8
Anton DahlstrÃ¶m <anton.dahlstrom508@gmail.com>
2016-09-14 12:32:22 +0200

    Add startup routine

--- fancontroller.c
+++ fancontroller.c
@@ -1,12 +1,24 @@
 #include <atmelrc.h>
 
+uint16_t fan1Current = 0;
+uint16_t fan2Current = 0;
+uint16_t fan3Current = 0;
+uint16_t fan4Current = 0;
+uint16_t fan5Current = 0;
+
+uint16_t fan1CurrentMaxSpeed = 0;
+uint16_t fan2CurrentMaxSpeed = 0;
+uint16_t fan3CurrentMaxSpeed = 0;
+uint16_t fan4CurrentMaxSpeed = 0;
+uint16_t fan5CurrentMaxSpeed = 0;
+
+uint8_t connectedFans = 0;
 
 int main (void) {
 
 	setup();
 
-	//startup routine
-		
+	startup();
 
 	while(1){
 
@@ -14,7 +26,7 @@ int main (void) {
 
 }
 
-void setup(void){
+void setup(void) {
 
 	cli(); //Disable interrupts temoprarily
 
@@ -31,7 +43,7 @@ void setup(void){
 			//Set fast-PWM mode
 			TCCR0A |= _BV(COM0A1) | _BV(COM0B1) | _BV(WGM00) | _BV(WGM01); //Timer0 PWM settings
 			TCCR1A |= _BV(COM1A1) | _BV(COM1B1) | _BV(WGM10) | _BV(WGM11); //Timer1 PWM settings
-			TCCR2A |= _BV(COM2A1) | _BV(COM2B1) | _BV(WGM20) | _BV(WGM21); //Timer2 PWM settings
+			TCCR2A |= _BV(COM2A1) | _BV(WGM20) | _BV(WGM21); //Timer2 PWM settings (only OC2A used)
 
 		//Set inputs for rotary encoder - all inputs with pull-ups
 			//PB0 button, PB4 Left. PB5 Right
@@ -41,15 +53,108 @@ void setup(void){
 			DDRD |= _BV(DDD0) |	_BV(DDD1) |	_BV(DDD2) |	_BV(DDD3) |	_BV(DDD4);
 
 		//ADC configuration
-			//REFS1 & REFS0 set to 0 as standard: external AFEF pin used as reference
+			//ADMUX: REFS1 & REFS0 set to 0 as standard, so external AREF pin is used as reference
 			ADCSRA |= _BV(ADPS0) | _BV(ADPS1) | _BV(ADPS2); //Set prescaler to 128 (125 kHz)
 			ADCSRA |= _BV(ADEN); //Enable ADC
 
 			//Disable digital inputs on Analog pins
 			DIDR0 |= _BV(ADC0) | _BV(ADC1) | _BV(ADC2) | _BV(ADC3) | _BV(ADC4);
 
-
-
 	sei(); //Enable interrupts again
+
+}
+
+/*
+	Startup routine
+		Sets all fans to 100% in order to ensure adequate startup power
+		Records the fans current consumption at 100% speed
+		Stores connection status
+*/
+void startup(void) {
+
+	//Activate all fans on full speed
+	FAN1PWM ^= 0;
+	FAN2PWM ^= 0;
+	FAN3PWM ^= 0;
+	FAN4PWM ^= 0;
+	FAN5PWM ^= 0;
+
+	//Let fans spin to full speed
+	_delay_ms(1000); 
+
+	//Read max currents
+	fan1CurrentMaxSpeed = readFanCurrent(FAN1CH);
+	fan2CurrentMaxSpeed = readFanCurrent(FAN2CH);
+	fan3CurrentMaxSpeed = readFanCurrent(FAN3CH);
+	fan4CurrentMaxSpeed = readFanCurrent(FAN4CH);
+	fan5CurrentMaxSpeed = readFanCurrent(FAN5CH);
+	fan1Current = fan1CurrentMaxSpeed;
+	fan2Current = fan2CurrentMaxSpeed;
+	fan3Current = fan3CurrentMaxSpeed;
+	fan4Current = fan4CurrentMaxSpeed;
+	fan5Current = fan5CurrentMaxSpeed;
+
+	checkConnection();
+
+}
+
+
+
+/*
+	Helper method to read all fan currents and store these in variables fanXcurrent
+*/
+void recordFanCurrents(void) {
+
+
+}
+
+
+
+/*
+	Helper method to read and return a voltage for a sepcific fan
+	Fan channels are recorded in variables FANxCH in .h file for easy reference
+*/
+uint16_t readFanCurrent(uint8_t chADC) {
+
+	ADMUX |= chADC;
+	ADCSRA |=_BV(ADSC); //Starts conversion
+
+	while( (ADCSR>>_BV(ADSC)) & 1); //Waits for conversion to finish
+
+	uint16_t out = 0;
+	out = ADCH<<sizeof(ADCL);
+	out |= ADCL;
+
+	return out;
+
+}
+
+
+
+/*
+	Helper method to record connection status of all fans
+	Uses the current reading of sensor to determine status
+*/
+void checkConnection(void) {
+
+	if(fan1Current > CONNECTIONTHRESHOLD){
+		connectedFans |= _BV(FAN1CONN);
+	}
+
+	if(fan2Current > CONNECTIONTHRESHOLD){
+		connectedFans |= _BV(FAN2CONN);
+	}
+
+	if(fan3Current > CONNECTIONTHRESHOLD){
+		connectedFans |= _BV(FAN3CONN);
+	}
+
+	if(fan4Current > CONNECTIONTHRESHOLD){
+		connectedFans |= _BV(FAN4CONN);
+	}
+
+	if(fan5Current > CONNECTIONTHRESHOLD){
+		connectedFans |= _BV(FAN5CONN);
+	}
 
 }
--- fancontroller.h
+++ fancontroller.h
@@ -8,6 +8,10 @@
 
 	//Function delcarations
 	void setup(void);
+	void startup(void);
+	void recordFanCurrents(void); 
+	uint8_t readFanCurrent(uint8_t channel);
+	void checkConnection(void);
 
 	#define SER PORTD0
 	#define SRCLK PORTD1
@@ -15,10 +19,24 @@
 	#define RCLK PORTD3
 	#define OE PORTD4
 
-	#define FAN0 0
-	#define FAN1 1
-	#define FAN2 2
-	#define FAN3 3
-	#define FAN4 4
+	#define FAN1CONN 0
+	#define FAN2CONN 1
+	#define FAN3CONN 2
+	#define FAN4CONN 3
+	#define FAN5CONN 4
+
+	#define FAN1PWM OCR0A
+	#define FAN2PWM OCR0B
+	#define FAN3PWM OCR1A
+	#define FAN4PWM OCR1B
+	#define FAN5PWM OCR0A
+
+	#define FAN1CH 0b00000000
+	#define FAN2CH 0b00000001
+	#define FAN3CH 0b00000010
+	#define FAN4CH 0b00000011
+	#define FAN5CH 0b00000100
+
+	#define CONNECTIONTHRESHOLD 2
 
 #endif
